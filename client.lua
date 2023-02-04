ebb6858b7bc7881b1bc38d2f098ae3e41c018444
Blu <113441527+BluGalaxies@users.noreply.github.com>
2023-02-04 15:07:57 +0000

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,41 @@
+RegisterCommand("+radiochatter", function(source, args, rawCommand)
+	TriggerEvent( 'Radio', source )
+end)
+
+RegisterCommand("-radiochatter", function(source, args, rawCommand)
+	TriggerEvent( 'Radio', source )
+end)
+
+
+RegisterKeyMapping('+radiochatter', '~g~Hold Radio', 'keyboard', 'LMENU')
+
+
+
+
+
+-- Credits to HelpIHaveEbola
+
+RegisterNetEvent('Radio')
+AddEventHandler('Radio', function()
+
+local ped = GetPlayerPed( -1 )
+    
+if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+
+Citizen.CreateThread( function()
+	RequestAnimDict( "random@arrests" )
+	   while ( not HasAnimDictLoaded( "random@arrests" ) ) do 
+                Citizen.Wait( 100 )
+            end
+				if IsEntityPlayingAnim(ped, "random@arrests", "generic_radio_chatter", 3) then
+				ClearPedSecondaryTask(ped)
+				SetCurrentPedWeapon(ped, GetHashKey("GENERIC_RADIO_CHATTER"), true)
+				else
+				TaskPlayAnim(ped, "random@arrests", "generic_radio_chatter", 8.0, 2.5, -1, 49, 0, 0, 0, 0 )
+				SetCurrentPedWeapon(ped, GetHashKey("GENERIC_RADIO_CHATTER"), true)
+            end 
+        end )
+    end 
+end )
+
+
